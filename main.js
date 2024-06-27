668dca1a3f21c9fdf0b7c842db75fb838d000299
matyash12 <hajekmatyas1@gmail.com>
2024-06-27 15:52:06 +0200

    works

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ const fs = require('fs');
 const path = require('path');
 const { Console } = require('console');
 
-var basepath = path.join(app.getAppPath() + "/..").toString();
+//var basepath = path.join(app.getAppPath() + "/..").toString(); //production
+var basepath = path.join(app.getAppPath() + "/Resources").toString(); //devel
+
 // Create a writable stream
 const logStream = fs.createWriteStream(path.join(basepath, 'app.log'), { flags: 'a' });
 
@@ -39,11 +41,11 @@ const steamSaveDirectoryDownload = basepath + '/downloadfolder'
 import { download, CancelError } from 'electron-dl';
 
 
-
+var win = null
 
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -60,6 +62,19 @@ app.whenReady().then(() => {
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
+
+
+
+
+    async function checkSteamInstallation() {
+        await waitOneSecond()
+        while (!check_if_steam_installed()) {
+            myConsole.log("Steam is not yet installed. Waiting one second before trying again.");
+            await waitOneSecond();
+        }
+    }
+    checkSteamInstallation();
+    
 })
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
@@ -73,8 +88,8 @@ function getWineCommandWithOverides(command_to_run) {
     myConsole.log("After running a command")
 }
 
-function runCommandWithExports(command_to_run){
-    const whole_command_to_run = "export WINEPREFIX=" + '"' + winePrefix + '"' + " && " + 'export WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" ' + " && "+ command_to_run
+function runCommandWithExports(command_to_run) {
+    const whole_command_to_run = "export WINEPREFIX=" + '"' + winePrefix + '"' + " && " + 'export WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" ' + " && " + command_to_run
     myConsole.log(whole_command_to_run)
     exec(whole_command_to_run)
     myConsole.log("After running a command")
@@ -85,11 +100,10 @@ function getSafePath(path) {
 }
 
 
-
-ipcMain.on('install_steam', async (event) => {
+async function install_steam() {
     myConsole.log("Install steam server")
     const downloadUrl = 'https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe';
-    
+
     const saveFilename = 'SteamSetup.exe'
     //const command = "curl " + url + " --output SteamSetup.exe";
 
@@ -108,13 +122,20 @@ ipcMain.on('install_steam', async (event) => {
             myConsole.error(error);
         }
     }
+}
+
+
+ipcMain.on('install_steam', async (event) => {
+    await install_steam();
 });
 
-
+async function start_steam_installer() {
+    getWineCommandWithOverides(steamInstallerExeFile)
+}
 ipcMain.on('start_steam_installer', async (event) => {
     //myConsole.log("Starting steam installer")
     //const wholeCommand = 'export WINEPREFIX="'+winePrefix+'"' +" && "+ '"'+ winePath+'"' + " " + '"'+steamInstallerExeFile+'"'
-    getWineCommandWithOverides(steamInstallerExeFile)
+    await start_steam_installer();
     //myConsole.log(wholeCommand)
     //exec(wholeCommand);
 });
@@ -141,20 +162,27 @@ ipcMain.on('wine_kill_all', async (event) => {
     myConsole.log("Killed all wine and wine-preloader")
 });
 
-ipcMain.on('create_wine_enviroment', async (event) => {
+async function create_wine_enviroment() {
     myConsole.log("Running wineboot")
     const wholeCommand = 'export WINEPREFIX="' + winePrefix + '"' + " && " + '"' + winePath + '"' + " " + "wineboot"
     myConsole.log(wholeCommand)
     exec(wholeCommand);
+}
+ipcMain.on('create_wine_enviroment', async (event) => {
+    await create_wine_enviroment();
 });
 
 
-ipcMain.on('move_wine_packages', async (event) => {
+async function move_wine_packages() {
     myConsole.log("Moving wine packages")
     const wholeCommand = "export WINEPREFIX=" + getSafePath(winePrefix) + " && " + "cp " + getSafePath(basepath + "/packages/dxvk-2.3.1/x64/") + "*.dll" + " " + getSafePath("$WINEPREFIX/drive_c/windows/system32") + " && " + "cp " + getSafePath(basepath + "/packages/dxvk-2.3.1/x32/") + "*.dll" + " " + getSafePath("$WINEPREFIX/drive_c/windows/syswow64");
     //const wholeCommand = 'export WINEPREFIX=" + " && "+ "cp "+basepath+"/packages/dxvk-2.3.1/x64/*.dll $WINEPREFIX/drive_c/windows/system32" + " && "+ "cp "+basepath+"/packages/dxvk-2.3.1/x32/*.dll $WINEPREFIX/drive_c/windows/syswow64"
     myConsole.log(wholeCommand)
     exec(wholeCommand);
+}
+
+ipcMain.on('move_wine_packages', async (event) => {
+    await move_wine_packages();
 });
 
 ipcMain.on('truckersmp_open_button', async (event) => {
@@ -164,4 +192,99 @@ ipcMain.on('truckersmp_open_button', async (event) => {
 
 });
 
+function Allow_other_easy_buttons(value) {
+    win.webContents.send("other_easy_buttons_allowed", value)
+}
 
+async function checkIfWineRunning() {
+    return new Promise((resolve, reject) => {
+        exec("ps aux | grep -i wine | grep -v grep", (error, stdout, stderr) => {
+            myConsole.log(error);
+            myConsole.log(stdout);
+            myConsole.log(stderr);
+            // If the output is empty, Wine is not running
+            resolve(stdout.trim().length > 0);
+        });
+    });
+}
+
+async function WaitForWine() {
+    await new Promise((resolve) => {
+        // Wait for 5 seconds before starting the checks
+        setTimeout(() => {
+            async function checkWine() {
+                const wineStopped = !(await checkIfWineRunning()); // Invert to check if Wine has stopped
+
+                if (wineStopped) {
+                    resolve();
+                } else {
+                    setTimeout(checkWine, 1000); // Check again after 1 second
+                }
+            }
+
+            checkWine();
+        }, 5000); // 5000 milliseconds = 5 seconds
+    });
+
+    console.log('Wine has stopped.');
+}
+
+function waitOneSecond() {
+    return new Promise(resolve => setTimeout(resolve, 1000));
+}
+
+/*
+Install Steam installer.
+Create wine enviroment
+Move dx11 file to wine
+Open Steam installer.
+Allow open Steam Button
+End
+*/
+ipcMain.on('easy_install', async (event) => {
+    myConsole.log("Running easy_install")
+
+    myConsole.log("Downloading Steam Installer.")
+    await install_steam();
+
+    myConsole.log("Creating Wine enviroment")
+    await create_wine_enviroment();
+
+    myConsole.log("Waiting for wine to finish")
+    await WaitForWine();
+    myConsole.log("Waiting for wine has Finished");
+
+    myConsole.log("Moving dx11 files to wine folder")
+    await move_wine_packages();
+
+    myConsole.log("Waitiing one second to finish")
+    await waitOneSecond();
+
+    myConsole.log("Opening Steam Installer")
+    await start_steam_installer();
+
+    myConsole.log("Check if installed")
+    while (check_if_steam_installed() == false){
+        myConsole.log("Steam is not yet installed waiting one second before trying again.")
+        await waitOneSecond();
+    }
+    
+
+    myConsole.log("Easy install Finished!")
+});
+
+
+
+//check if wine is already installed
+function check_if_steam_installed() {
+    myConsole.log("CHecking if steam exe file exists")
+    if (fs.existsSync(steamExeFile)) {
+        myConsole.log("Steam exe file exists")
+        Allow_other_easy_buttons(true)
+        return true;
+    } else {
+        myConsole.log("Steam exe file does not exists")
+        Allow_other_easy_buttons(false)
+        return false;
+    }
+}
--- renderer.js
+++ renderer.js
@@ -11,31 +11,77 @@ const truckersmp_open_button = document.getElementById('truckersmp_open_button')
 const create_wine_enviroment = document.getElementById('create_wine_enviroment');
 const move_wine_packages = document.getElementById('move_wine_packages');
 
+const open_advanced = document.getElementById('open_advanced');
+const close_advanced = document.getElementById('close_advanced');
+const advanced_div = document.getElementById('advanced_div');
 
-steam_install_button.addEventListener('click', () => {
-    myConsole.log('Hello World!');
+//easy controls
+const easy_install_button = document.getElementById('easy_install_button');
+const easy_open_steam_button = document.getElementById('easy_open_steam_button');
+const easy_open_truckersmp_button = document.getElementById('easy_open_truckersmp_button');
+
+easy_install_button.addEventListener('click', () => {
+    ipcRenderer.send('easy_install');
+});
+easy_open_steam_button.addEventListener('click', () => {
+    ipcRenderer.send('start_steam');
+});
+easy_open_truckersmp_button.addEventListener('click', () => {
     ipcRenderer.send('install_steam');
 });
 
-steam_installer_open_button.addEventListener('click', () =>{
+
+
+
+
+
+
+steam_install_button.addEventListener('click', () => {
+    ipcRenderer.send('install_steam');
+});
+
+steam_installer_open_button.addEventListener('click', () => {
     ipcRenderer.send('start_steam_installer')
 });
 
-steam_open_button.addEventListener('click', () =>{
+steam_open_button.addEventListener('click', () => {
     ipcRenderer.send('start_steam')
 });
-wine_kill_all.addEventListener('click', () =>{
+wine_kill_all.addEventListener('click', () => {
     ipcRenderer.send('wine_kill_all')
 });
 
-create_wine_enviroment.addEventListener('click', () =>{
+create_wine_enviroment.addEventListener('click', () => {
     ipcRenderer.send('create_wine_enviroment')
 });
 
-move_wine_packages.addEventListener('click', ()=>{
+move_wine_packages.addEventListener('click', () => {
     ipcRenderer.send('move_wine_packages')
 });
 
-truckersmp_open_button.addEventListener('click', ()=>{
+truckersmp_open_button.addEventListener('click', () => {
     ipcRenderer.send('truckersmp_open_button')
+});
+
+
+//advanced_div control
+open_advanced.addEventListener('click', () => {
+    advanced_div.hidden = false
+});
+close_advanced.addEventListener('click', () => {
+    advanced_div.hidden = true
+});
+
+
+//recievign from backend
+ipcRenderer.on('other_easy_buttons_allowed', (event, data) => {
+    if (data == true){
+        easy_install_button.disabled = true;
+        easy_open_steam_button.disabled = false;
+        easy_open_truckersmp_button.disabled = false;
+    }else{
+        easy_install_button.disabled = true
+        easy_open_steam_button.disabled = true;
+        easy_open_truckersmp_button.disabled = true;
+    }
 });
